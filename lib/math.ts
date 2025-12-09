cce9e9ebee69ed2001944efdad11f81cdbab8c5e
Pipe0105 <juanfelipegomez0105@gmail.com>
2025-12-09 16:03:19 -0500

    Eliminando contenido viejo para reemplazarlo con la versión nueva

--- lib/math.ts
+++
@@ -1,3 +0,0 @@
-export const round4 = (n: number) => Math.round(n * 1e4) / 1e4; // guardamos con 4 decimales
-export const show2 = (n: number) =>
-  new Intl.NumberFormat("es-CO", { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(n);
--- types/dom-to-image-more.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "dom-to-image-more" {
-  const domtoimage: {
-    toPng(node: HTMLElement, options?: any): Promise<string>;
-    toJpeg(node: HTMLElement, options?: any): Promise<string>;
-    toSvg(node: HTMLElement, options?: any): Promise<string>;
-  };
-  export default domtoimage;
-}
